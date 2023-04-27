d9c151a88dee2878818e7502b791df3d6bbe10eb
shira-chesler <shira.chesler@gmail.com>
2023-04-27 21:32:18 +0300

    added functions to betterping

--- betterping.py
+++ betterping.py
@@ -6,7 +6,7 @@ import time
 import threading
 
 from watchdog import create_watchdog_tcp_socket
-from ping import create_packet, send_ping, ICMP_ECHO_REPLY
+from ping import ICMP_ECHO_REPLY, ICMP_ECHO_REQUEST, ICMP_ECHO_CODE
 
 # constants
 WATCHDOG_PORT = 3000
@@ -14,13 +14,32 @@ WATCHDOG_IP = 'localhost'
 
 # global
 host = 0
+seq = 0
 
 
-"""
+def generate_checksum(packet) -> int:
+    """
+    this code is based on the generate checksum code from the Moodle in C converted to python
+    :param packet: packet to calculate checksum to
+    :return: packet checksum
+    """
+    checksum = 0
+    count_to = (len(packet) // 2) * 2
+    for i in range(0, count_to, 2):
+        checksum += (packet[i] << 8) + packet[i + 1]
+    if count_to < len(packet):
+        checksum += packet[len(packet) - 1] << 8
+    checksum = (checksum >> 16) + (checksum & 0xFFFF)
+    checksum += checksum >> 16
+    return (~checksum) & 0xFFFF
 
 
 # Create an ICMP packet
 def create_packet():
+    """
+    creates a packet to send
+    :return: the data, and the packet created
+    """
     global seq
     seq += 1
     # Generate a dummy header with a 0 checksum
@@ -36,12 +55,17 @@ def create_packet():
 
 
 # Send an ICMP packet to the specified host
-def send_ping(sock, packet):
+def send_ping(raw_socket, packet):
+    """
+     sends packet to host
+     :param raw_socket: raw socket that sends
+     :param packet: the packet to sends
+     """
     try:
-        sock.sendto(packet, (host, 0))
+        raw_socket.sendto(packet, (host, 1))
     except socket.error:
         print('Error: Failed to send packet')
-        sys.exit()"""
+        sys.exit()
 
 
 def recv_ping(betterping_socket):
@@ -96,19 +120,16 @@ def ping_flow(betterping_socket, watchdog_thread) -> None:
 
     status = True
     first_send = True
-
     try:
         while watchdog_thread.is_alive():
             data, packet = create_packet()
             send_ping(raw_socket, packet)
-            if first_send:
+            if first_send is True:
                 print(f'PING', host, f'({host})', f'{len(data)} data bytes')
                 first_send = False
-
             # if got a reply - sends alive message to the watchdog
-            if status:
+            if status is True:
                 betterping_socket.send("ping".encode())
-
             statistics = recv_ping(raw_socket)
             if statistics != 0:
                 print(statistics)
